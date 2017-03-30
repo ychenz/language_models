c6c3242d8a235fb564dc2fad12e7ab533ee2a762
yzhao <happyzychjl@gmail.com>
2017-03-29 22:06:06 -0400

    Fixed sv.saver request feed issue   - By passing summary_op=None when initializing supervisor or sv will call summary ops which depends on feed_dict, which is causing failure of saving Changed adam optimizer to gradient descent which is more memory effective Added automatic decay of learning rate Tuned model

--- RNNModel_TF.py
+++ RNNModel_TF.py
@@ -9,6 +9,7 @@ import csv
 import multiprocessing as mp
 import numpy as np
 import time
+import math
 
 # Download nltk corpus and tagger
 # nltk.download('punkt')
@@ -18,15 +19,20 @@ logging = tf.logging
 
 class Config(object):
     vocabulary_size = 8000
-    batch_size = 20
+    batch_size = 40
     embedding_size = hidden_size = 200  # size of word's embedding vector
     num_layers = 2  # not used, single layer for now
     lr_decay = 0.8
     keep_prob = 0.5  # rate of keeping a neuron for dropout operation
-    init_scale = 0.1
-    learning_rate = 0.005
-    max_grad_norm = 10  #??
+
+    init_scale = 0.1  #random uniform initializer
+
+    learning_rate = 0.5
+    max_grad_norm = 5  # prevent exploding gradient issue
     max_epoch = 5
+
+    def get_std(self):
+        return math.sqrt(2/self.vocabulary_size)
 
 
 class RedditParser(object):
@@ -44,6 +50,7 @@ class RedditParser(object):
         self.word_to_index = None
         self.X = None
         self.Y = None
+        self.max_len = 0
 
         self.cursor = 0
         self.epoch = 0
@@ -75,7 +82,7 @@ class RedditParser(object):
         '''
         # print("Tokenize comment to sentences")
         sentences = itertools.chain(*[nltk.sent_tokenize(body)])
-        sentences = ["%s %s %s" % (self.sentence_start_token, x, self.sentence_end_token) for x in sentences]
+        sentences = ["%s %s %s" % (self.sentence_start_token, x, self.sentence_end_token) for x in sentences if len(x) < 300]  #0.5% of sentence has length >300 
         return sentences
 
     def create_vocab(self):
@@ -97,9 +104,9 @@ class RedditParser(object):
                         for row in reader:
                             body = row[0]
                             raw_sententces.append(body.lower())
-                            # i += 1
-                            # if i >= 1000: # for testing the graph with small data
-                            #     break
+                            i += 1
+                            if i >= 50000: # for testing the graph with small data
+                               break
                         pool = mp.Pool(processes=8)
                         # split comments to sentences
                         all_sentences = pool.map(self._sent_tokenization, raw_sententces)
@@ -143,7 +150,7 @@ class RedditParser(object):
         print("Generating words lookup table ...")
         if os.path.exists("models/word_labels.csv"):
             os.remove("models/word_labels.csv")
-        with open("models/word_labels.csv",'w+') as f:
+        with open("models/word_labels.csv", 'w+') as f:
             writer = csv.writer(f)
             for id_word in enumerate(index_to_word):
                 writer.writerow(id_word)
@@ -151,8 +158,10 @@ class RedditParser(object):
 
         self.index_to_word = index_to_word
         self.word_to_index = word_to_index
+        self.max_len = len(max(X_train, key=len))
         self.X = X_train
         self.Y = y_train
+        print("Max length: %d" % self.max_len)
 
     def shuffle(self):
         shuffled_indices = np.arange(len(self.X))
@@ -161,6 +170,7 @@ class RedditParser(object):
         new_Y = [ self.Y[i] for i in shuffled_indices ]
         self.X = new_X
         self.Y = new_Y
+
 
     def next_batch(self):
         '''
@@ -181,7 +191,7 @@ class RedditParser(object):
 
         # padding
         seq_len = []
-        maxlen = max([len(sent) for sent in batch_x])
+        maxlen =self.max_len
         for sent_x,sent_y in zip(batch_x,batch_y):
             seq_len.append(len(sent_x))
             diff = maxlen - len(sent_x)
@@ -203,19 +213,23 @@ class RedditParser(object):
 
 class GenerativeGRUModel(object):
 
-    def __init__(self,config,is_training):
+    def __init__(self,config,is_training,max_len):
         self.config = config
         state_size = self.config.hidden_size
         keep_prob = self.config.keep_prob
         vocabulary_size = self.config.vocabulary_size
         batch_size = self.config.batch_size
 
-        self.seq_len = seq_len = tf.placeholder(tf.int32, shape=(batch_size,), name="seq_len")
-        self.inputs = inputs = tf.placeholder(tf.int32, shape=(batch_size, None), name="inputs")
-        self.labels = labels = tf.placeholder(tf.int32, shape=(batch_size, None), name="labels")
+        self._seq_len = tf.placeholder(tf.int32, shape=(batch_size,), name="seq_len")  # holds length of each input sentence of the batch
+        self._inputs = tf.placeholder(tf.int32, shape=(batch_size, max_len), name="inputs")
+        self._labels = tf.placeholder(tf.int32, shape=(batch_size, max_len), name="labels")
+        # these lines are causing memory fragmentation, variable shape is causing lots of tensor reallocation
+        # self._inputs = tf.placeholder(tf.int32, shape=(batch_size, None), name="inputs")
+        # self._labels = tf.placeholder(tf.int32, shape=(batch_size, None), name="labels")
 
         # Building RNN graph
-        GRUCell = tf.contrib.rnn.GRUCell(state_size)
+        with tf.variable_scope("RNN"):
+            GRUCell = tf.contrib.rnn.GRUCell(state_size)
 
         if is_training and keep_prob < 1:
             attn_cell = tf.contrib.rnn.DropoutWrapper(
@@ -232,7 +246,7 @@ class GenerativeGRUModel(object):
         with tf.device("/cpu:0"):
             embedding = tf.get_variable(
                 "embedding", [vocabulary_size, state_size], dtype=tf.float16)
-            inputs = tf.nn.embedding_lookup(embedding, inputs)
+            inputs = tf.nn.embedding_lookup(embedding, self._inputs)
 
         if is_training and keep_prob < 1:
             inputs = tf.nn.dropout(inputs, keep_prob)
@@ -240,8 +254,8 @@ class GenerativeGRUModel(object):
         #  forward pass
         # rnn_outputs shape: [batch_size x max(seq_len) x state_size]  State shape: [batch_size x state_size]
         with tf.variable_scope("RNN"):
-            rnn_outputs, state = tf.nn.dynamic_rnn(cell, inputs, sequence_length=seq_len,
-                                                         initial_state=self._initial_state)
+            rnn_outputs, state = tf.nn.dynamic_rnn(cell, inputs, sequence_length=self._seq_len,
+                                                   initial_state=self._initial_state)
         rnn_outputs = tf.reshape(tf.concat(axis=1, values=rnn_outputs), [-1, state_size])  # shape: [（batch_size*max(seq_len)）x state_size]
         softmax_w = tf.get_variable(
             "softmax_w", [state_size, vocabulary_size], dtype=tf.float16)
@@ -252,9 +266,9 @@ class GenerativeGRUModel(object):
         self._predictions = tf.nn.softmax(logits)
 
         # convert labels to one-hot vectors
-        labels = tf.one_hot(tf.reshape(labels, [-1]),depth=vocabulary_size,axis=-1)
-        loss = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels,name=None)
-        self._cost = cost = tf.reduce_sum(loss) / tf.reduce_sum(tf.cast(seq_len, tf.float16))  # average loss across all valid predictions
+        labels = tf.one_hot(tf.reshape(self._labels, [-1]), depth=vocabulary_size, axis=-1)
+        loss = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels, name=None)
+        self._cost = cost = tf.reduce_sum(loss) / tf.reduce_sum(tf.cast(self._seq_len, tf.float16))  # average loss across all valid predictions
         self._final_state = state
 
         if not is_training:
@@ -263,9 +277,32 @@ class GenerativeGRUModel(object):
         self._lr = tf.Variable(config.learning_rate, trainable=False)
         tvars = tf.trainable_variables()
         grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars), config.max_grad_norm)
-        optimizer = tf.train.AdamOptimizer(self._lr,epsilon=1e-4)  # slightly larger epsilon to avoid numerical instability with zero moments with float16
+        optimizer = tf.train.GradientDescentOptimizer(self._lr)  # slightly larger epsilon to avoid numerical instability with zero moments with float16
         self._train_op = optimizer.apply_gradients(
             zip(grads, tvars))
+
+        tf.summary.scalar("Training Loss", self._cost)
+        tf.summary.scalar("Learning Rate", self._lr)
+        self._summary_op = tf.summary.merge_all()
+
+        self._new_lr = tf.placeholder(
+            tf.float32, shape=[], name="new_learning_rate")
+        self._lr_update = tf.assign(self._lr, self._new_lr)
+
+    def assign_lr(self, session, lr_value):
+        session.run(self._lr_update, feed_dict={self._new_lr: lr_value})
+
+    @property
+    def seq_len(self):
+        return self._seq_len
+
+    @property
+    def input(self):
+        return self._inputs
+
+    @property
+    def labels(self):
+        return self._labels
 
     @property
     def initial_state(self):
@@ -288,6 +325,10 @@ class GenerativeGRUModel(object):
         return self._train_op
 
     @property
+    def summary_op(self):
+        return self._summary_op
+
+    @property
     def lr(self):
         return self._lr
 
@@ -295,25 +336,31 @@ class GenerativeGRUModel(object):
 def main(_):
     config = Config()
     with tf.Graph().as_default():
-        initializer = tf.random_uniform_initializer(-config.init_scale,config.init_scale)
-        config.batch_size = 5
-        config.vocabulary_size = 8000
-        config.max_epoch = 5
-        config.learning_rate = 0.007
+        config.batch_size = 45  # This config takes about 1500M memory with max 300 sentence
+        config.learning_rate = 0.5
+        initializer = tf.truncated_normal_initializer(stddev=config.get_std())
 
         with tf.name_scope("Train"):
             reddit_parser = RedditParser(config)
             with tf.variable_scope("Model", reuse=None, initializer=initializer):
-                model = GenerativeGRUModel(config, True)
-            tf.summary.scalar("Training Loss", model.cost)
+                model = GenerativeGRUModel(config, True, reddit_parser.max_len)
 
         # train the model
         start_time = time.time()
-        loss_value = 0
         batch_cnt = 0
-        sv = tf.train.Supervisor(logdir='checkpoints')
-        with sv.managed_session() as session:
+        loss_value = 0
+        prev_cost = 0
+
+        gpu_config = tf.ConfigProto()
+        gpu_config.gpu_options.allow_growth = True
+        save_path = '/home/tina/Scripts/python/RNN/checkpoints/'
+        sv = tf.train.Supervisor(logdir='checkpoints',summary_op=None)
+        with sv.managed_session(config=gpu_config) as session:
+
             while reddit_parser.epoch < config.max_epoch:
+                # if sv.should_stop():
+                #     break
+
                 batch_data = reddit_parser.next_batch()  # generate batch of training data
                 if batch_data is None:
                     print("Cost at epoch %d: %f" % (reddit_parser.epoch, loss_value))
@@ -322,18 +369,36 @@ def main(_):
                 else:
                     x, y, seq_len = batch_data
 
+                session.run(model.initial_state)
                 feed_dict = {
                     model.seq_len: seq_len,
-                    model.inputs: x,
+                    model.input: x,
                     model.labels: y,
                 }
+
                 _, loss_value = session.run([model.train_op, model.cost], feed_dict=feed_dict)
-                print("Cost at batch %d: %f" % (batch_cnt, loss_value))
+                # print("Cost at batch %d: %f" % (batch_cnt, loss_value))
                 batch_cnt += 1
 
-            save_path = './models/rnn-model'
+                # adjusting learning rate if overshoot
+                if prev_cost != 0 and loss_value - prev_cost > 0.5:
+                    print("Cost at batch %d: %f increased from %f" % (batch_cnt, loss_value, prev_cost))
+                    lr_rate = config.learning_rate * config.lr_decay
+                    print("Decreasing learning rate from %f to %f" % (config.learning_rate, lr_rate))
+                    model.assign_lr(session, lr_rate)
+                    config.learning_rate = lr_rate
+                prev_cost = loss_value
+
+                if batch_cnt % 500 == 0 and batch_cnt != 0:
+                    summaries = session.run(model.summary_op,feed_dict=feed_dict)
+                    sv.summary_computed(session, summaries)
+                    print("Cost at batch %d: %f" % (batch_cnt, loss_value))
+                    print("Saving model to %s" % save_path)
+                    sv.saver.save(session, save_path, global_step=sv.global_step)
+
             print("Saving model to %s" % save_path)
             sv.saver.save(session, save_path, global_step=sv.global_step)
+
         # state = session.run(model.initial_state)
         # pred = session.run(model.predictions)
         # cost = session.run(model.cost)
